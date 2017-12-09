708baceac19b3ee6da4161ad3b694a8eecc80c9b
Dariel Barroso <dariel_games@yahoo.com>
2017-12-08 20:52:32 -0500

    refactored controllre

---
+++ LEDWindows/ColorConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEDController
+{
+    public class ColorConverter
+    {
+        public const int COLOR_DEPTH = 255;
+
+        [DllImport("shlwapi.dll")]
+        public static extern int ColorHLSToRGB(int H, int L, int S);
+        public static Color GetImageColor(Bitmap Image)
+        {
+            var scaled = Image;
+            
+            int colorDepth = 256;
+            int[] colors = new int[colorDepth];
+            float bucketNumber = 360 / colorDepth;
+
+            for (int x = 0; x < scaled.Size.Width; x++)
+            {
+                for (int y = 0; y < scaled.Size.Height; y++)
+                {
+                    float hue = scaled.GetPixel(x, y).GetHue();
+                    int colorIndex = (int)Math.Round(hue / bucketNumber);
+
+                    float saturation = scaled.GetPixel(x, y).GetSaturation();
+                    float brightness = scaled.GetPixel(x, y).GetBrightness();
+
+                    colorIndex = colorIndex % colorDepth;
+
+                    if (brightness >= 0.10f && brightness <= 0.80f && saturation >= 0.3f)
+                        colors[colorIndex]++;
+
+                }
+            }
+
+            int colorMax = 0;
+            for (int i = 0; i < colors.Length; i++)
+                if (colors[i] > colors[colorMax])
+                    colorMax = i;
+
+            return ColorTranslator.FromWin32(ColorHLSToRGB(255 * (colorMax * (int)bucketNumber) / 360, 132, 240));
+        }
+
+    }
+}
--- LEDWindows/Form1.cs
+++ LEDWindows/Form1.cs
@@ -5,43 +5,60 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Threading.Tasks;
 
 namespace LEDController
 {
     public partial class Form1 : Form
     {
-        public ArduinoController controller;
-        private Timer timer = new Timer();
-
-
+        private Color currectColor;
+        private Timer timer;
+        private Bitmap screen;
+        Bitmap scaled;
+        private Graphics gfxScreenshot;
+        private Graphics gfxScaled;
         public Form1()
         {
             InitializeComponent();
 
-            controller = new ArduinoController();
-            controller.Start();
+            screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            scaled = new Bitmap(160, 90);
+            gfxScreenshot = Graphics.FromImage(screen);
 
-            timer.Interval = 33;
-            timer.Tick += UpdateLabel;
-            timer.Tick += UpdatePictureBox;
+            timer = new Timer();
+            timer.Tick += updateGUI;
             timer.Start();
-            
-        }
 
-        void CloseWorkerThread(object sender, EventArgs e)
-        {
-            controller.Stop();
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    update();
+                    Task.Delay(33);
+                }
+            });
         }
-        void UpdateLabel(object sender, EventArgs e)
+        void updateGUI(object sender, EventArgs e)
         {
             label1.Text = "Memory: " + System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / 1048576 + "mb";
-        }
-        void UpdatePictureBox(object sender, EventArgs e)
-        {
             Bitmap img = new Bitmap(400, 400, PixelFormat.Format32bppArgb);
             Graphics g = Graphics.FromImage(img);
-            g.DrawLine(new Pen(controller.CurrentColor, 300), new Point(0, 0), new Point(300, 300));
+            g.DrawLine(new Pen(this.currectColor, 300), new Point(0, 0), new Point(300, 300));
             pictureBox1.Image = img;
+
+        }
+        void update()
+        {
+            screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            gfxScreenshot = Graphics.FromImage(screen);
+
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size,
+                                             CopyPixelOperation.SourceCopy);
+            gfxScaled = Graphics.FromImage(scaled);
+            gfxScaled.DrawImage(screen, new RectangleF(0, 0, scaled.Width, scaled.Height));
+
+
+            this.currectColor = ColorConverter.GetImageColor(scaled);
         }
 
 
